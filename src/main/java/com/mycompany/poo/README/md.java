b48680656cf51ab9cb363a34f95962a49ebae484
Carol Pereira <130416799+carol9074@users.noreply.github.com>
2023-05-18 21:56:57 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/poo/README/md.java
@@ -0,0 +1,30 @@
+/*
+Documento do codigo em Portugues/Brasil
+
+SISTEMA BANCÁRIO EM POO:
+
+O codigo a seguir simula um sistema bancário basico em java.
+No entanto, contém dois menus principais:
+* Cadastro() Onde o usuário se cadastra com algumas informações necessárias como: nome, CPF e número do celular.
+* Login() Onde o usuário insere seu CPF. Logo abaixo haverá outro menu para poder realizar outras tarefas como:
+Atualizar cadastro() Esta função permite que você modifique seu celular.
+Vizualizar perfil() Mostra na tela o cadastro completo e o saldo do usuário.
+Depositar() Esta função permite que faça um deposito e adicione esse valor na conta do usuário.
+Sacar() Permite que o usuário possa sacar um valor determinado,após o saque realizado  automaticamento o sistema atualiza o seu saldo atual.
+Remover conta() Deleta o usuário do nosso sistema,apos isso não tera mais acesso.
+
+
+Code document in English
+
+BANKING SYSTEM IN POO:
+
+The following code simulates a basic banking system in java.
+However, it contains two main menus:
+* Register() Where the user registers with some necessary information such as: name, CPF and cell phone number.
+* Login() Where the user enters his CPF. Just below there will be another menu to be able to perform other tasks such as:
+Update registration() This function allows you to modify your cell phone.
+View profile() Shows the users complete registration and balance on the screen.
+Deposit() This function allows you to make a deposit and add that amount to the users account.
+Withdraw() Allows the user to withdraw a certain amount, after the withdrawal is carried out automatically, the system updates its current balance.
+Remove account() Deletes the user from our system, after that he will no longer have access.
+*/
